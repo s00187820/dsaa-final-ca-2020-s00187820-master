d97c05e34bf2d501d1bdd71d70403d0a494301aa
CianO'Reilly <46957507+GitSmugg@users.noreply.github.com>
2020-05-21 16:14:21 +0100

    player move and shoot

--- DSAAFCA2020/Game1.cs
+++ DSAAFCA2020/Game1.cs
@@ -5,9 +5,7 @@ using Microsoft.Xna.Framework.Input;
 
 namespace DSAAFCA2020
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
+    
     public class Game1 : Game
     {
         GraphicsDeviceManager graphics;
@@ -15,7 +13,16 @@ namespace DSAAFCA2020
         string ID = Activity.StudentID;
         string Name = Activity.Name;
         private SpriteFont font;
-        string Message = "Starting Development";
+        string Message = "Basic Movement";
+
+        Texture2D background;
+        Texture2D ship;
+        Texture2D shoot;
+
+        Vector2 shipPosition;
+        Vector2 shootPosition;
+        Vector2 shootOffset;
+        bool hasShoot = false;
 
         public Game1()
         {
@@ -23,12 +30,7 @@ namespace DSAAFCA2020
             Content.RootDirectory = "Content";
         }
 
-        /// <summary>
-        /// Allows the game to perform any initialization it needs to before starting to run.
-        /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
-        /// and initialize them as well.
-        /// </summary>
+       
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
@@ -36,51 +38,86 @@ namespace DSAAFCA2020
             base.Initialize();
         }
 
-        /// <summary>
-        /// LoadContent will be called once per game and is the place to load
-        /// all of your content.
-        /// </summary>
+        
         protected override void LoadContent()
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
             font = Content.Load<SpriteFont>("TextFont");
             // TODO: use this.Content to load your game content here
+
+            ship = Content.Load<Texture2D>("monoShip");
+            background = Content.Load<Texture2D>("background");
+            shoot = Content.Load<Texture2D>("bullet");
+
+            shootOffset = new Vector2(ship.Width / 15, ship.Height / 10);
+            shootOffset += new Vector2(-5, 0);
         }
 
-        /// <summary>
-        /// UnloadContent will be called once per game and is the place to unload
-        /// game-specific content.
-        /// </summary>
+      
         protected override void UnloadContent()
         {
             // TODO: Unload any non ContentManager content here
         }
 
-        /// <summary>
-        /// Allows the game to run logic such as updating the world,
-        /// checking for collisions, gathering input, and playing audio.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+       
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+            Vector2 movement = Vector2.Zero;
+            KeyboardState keystate = Keyboard.GetState();
+            
+            if (keystate.IsKeyDown(Keys.Right))
+            {
+                movement.X += 1;
+            }
+            if (keystate.IsKeyDown(Keys.Left))
+            {
+                movement.X -= 1;
+            }
+            if (keystate.IsKeyDown(Keys.Up))
+            {
+                movement.Y -= 1;
+            }
+            if (keystate.IsKeyDown(Keys.Down))
+            {
+                movement.Y += 1;
+            }
+            if (keystate.IsKeyDown(Keys.Space))
+            {
+                shootPosition = shipPosition + shootOffset;
+                hasShoot = true;
+            }
+
+            shipPosition += movement;
+
+            if (hasShoot)
+            {
+                shootPosition.Y -= 5;
+                if (shootPosition.Y > GraphicsDevice.Viewport.Width)
+                {
+                    hasShoot = false;
+                }
+               
+            }
 
             base.Update(gameTime);
         }
 
-        /// <summary>
-        /// This is called when the game should draw itself.
-        /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+       
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
             spriteBatch.Begin();
-            spriteBatch.DrawString(font, Message + " " + ID + " " + Name, new Vector2(10, 10), Color.White);
+            spriteBatch.Draw(background, GraphicsDevice.Viewport.Bounds, Color.White);
+            spriteBatch.DrawString(font, Message + " s00187820" + ID +
+                "CianOReilly " + Name, new Vector2(10, 10), Color.White);
+            if (hasShoot) spriteBatch.Draw(shoot, shootPosition, Color.White);
+            spriteBatch.Draw(ship, shipPosition, Color.White);
+            
             spriteBatch.End();
             // TODO: Add your drawing code here
 
